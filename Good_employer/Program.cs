35d8e90ea8fee6fae0a8183dd60fcfc6535b4aee
vilko <helgi8@mail.ru>
2018-10-09 21:12:23 +0300

    Добавьте файлы проекта.

---
+++ Good_employer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Good_employer
+{
+    class Program
+    {
+        private static string GetGreetingMessage(string name, double salary)
+        {
+            // возвращает "Hello, <name>, your salary is <salary>"
+            salary = Math.Ceiling(salary);
+            return "Hello, "+name+", your salary is "+salary;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetGreetingMessage("Student", 10.01));
+            Console.WriteLine(GetGreetingMessage("Bill Gates", 10000000.5));
+            Console.WriteLine(GetGreetingMessage("Steve Jobs", 1));
+        }
+    }
+}
